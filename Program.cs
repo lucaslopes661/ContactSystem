8abea4e1047eb35bcb6f936f48be25a4b909f91f
Lucas <lucaslopesgrisolia@gmail.com>
2026-03-20 14:21:22 -0300

    Primeiro commit, classe contatos e menu configurados apenas

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ContactSystem
+{
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Contatos> c = new List<Contatos>();
+            int opcao;
+            bool validacaoEntrada
+
+
+            do
+            {
+                Console.WriteLine("-----Menu-----");
+                Console.WriteLine("1 - Criar novo contato.");
+                Console.WriteLine("2 - Lista de contatos.");
+                Console.WriteLine("3 - Editar contatos.");
+                Console.WriteLine("4 - Novos contatos.");
+                validacaoEntrada = int.TryParse(Console.ReadLine(), out opcao);
+            } while (validacaoEntrada is not true);
+
+
+            
+
+        }
+    }
+}

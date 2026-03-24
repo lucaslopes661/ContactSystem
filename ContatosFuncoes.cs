7a50c9b4bce4ba58d9f73b925aa13630ed31fbab
Lucas <lucaslopesgrisolia@gmail.com>
2026-03-24 15:08:48 -0300

    Menu configurado, e funções editar, remover, salvar e carregar criadas

--- ContatosFuncoes.cs
+++ ContatosFuncoes.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ContactSystem
 {
     internal class ContatosFuncoes
     {
         List<Contatos> c = new List<Contatos>();
-      
-        Contatos co = new Contatos();
+
+        string caminho = "C:\\Users\\Lucas\\Desktop\\vs\\ContactSystem";
+
 
         public void Menu()
         {
+            CarregaContatos();
+
             int opcaoMenu;
             bool validacaoEntrada;
 
@@ -51,14 +57,19 @@ namespace ContactSystem
                         AdicionaContato();
                         break;
                     case 2:
-                        Console.WriteLine("Selecionada a opção 2");
-                        Console.ReadKey();
+                        ListaContato();
                         break;
                     case 3:
-                        Console.WriteLine("Selecionada a opção 3");
+                        EditaContato();
                         break;
                     case 4:
-                        Console.WriteLine("Selecionada a opção 4");
+                        RemoverContato();
+                        break;
+                    case 0:
+                        Console.WriteLine("Programa encerrado.");
+
+                        Console.WriteLine("Clique qualquer tecla para fechar");
+
                         break;
                     default:
                         Console.WriteLine("Selecione uma opção válida.");
@@ -88,6 +99,8 @@ namespace ContactSystem
         public void AdicionaContato()
         {
 
+            Contatos co = new Contatos();
+
             Console.Clear();
 
             Console.WriteLine("Digite o nome do seu contato: ");
@@ -99,7 +112,22 @@ namespace ContactSystem
             Console.WriteLine("Digite o email do seu contato: ");
             co.email = Console.ReadLine();
 
-            c.Add(co);
+            try
+            {
+                c.Add(co);
+
+                Console.WriteLine("Contato salvo com sucesso");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            SalvaContatos();
+
+            Console.WriteLine("Clique qualquer tecla para retornar ao menu.");
+            Console.ReadKey();
+
 
 
         }
@@ -111,7 +139,7 @@ namespace ContactSystem
             if (c.Count == 0)
             {
                 Console.WriteLine("Você não possui contatos.");
-
+                Console.WriteLine("Clique qualquer tecla para retornar ao menu.");
                 Console.ReadKey();
             }
             else 
@@ -131,6 +159,178 @@ namespace ContactSystem
                 Console.ReadKey();
 
             }
+
+        }
+
+        public void EditaContato()
+        {
+            Console.Clear();
+            bool opcaoValida;
+            bool opcaoValida2;
+            int opcaoEdita;
+            int contato;
+            int contatoCorrigido;
+
+            if (c.Count == 0)
+            {
+                Console.WriteLine("Você não possui contatos.");
+                Console.WriteLine("Clique qualquer tecla para retornar ao menu.");
+                Console.ReadKey();
+            }
+            else
+            {
+                do {
+                    Console.WriteLine("Qual contato deseja editar?");
+                    opcaoValida = int.TryParse(Console.ReadLine(), out contato);
+
+                    if (opcaoValida is not true)
+                    {
+                        Console.WriteLine("Digite o número do contato que deseja editar.");
+                    }
+                }while (opcaoValida is not true);
+
+                contatoCorrigido = contato - 1;
+
+                do
+                {
+                    Console.WriteLine("---O que você deseja editar?---");
+                    Console.WriteLine("1 - Nome");
+                    Console.WriteLine("2 - Telefone");
+                    Console.WriteLine("3 - Email");
+                    opcaoValida2 = int.TryParse(Console.ReadLine(), out opcaoEdita);
+
+                    if (opcaoValida2 is not true)
+                    {
+                        Console.WriteLine("Digite o número da opção desejada");
+                    }
+                } while (opcaoValida2 is not true);
+
+
+                switch (opcaoEdita)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o novo nome: ");
+                        c[contatoCorrigido].nome = Console.ReadLine();
+                        Console.WriteLine("Nome editado com sucesso.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Digite o novo telefone: ");
+                        c[contatoCorrigido].telefone = Console.ReadLine();
+                        Console.WriteLine("Telefone editado com sucesso.");
+                        break;
+                    case 3:
+                        Console.WriteLine("Digite o novo email: ");
+                        c[contatoCorrigido].email = Console.ReadLine();
+                        Console.WriteLine("Email editado com sucesso. ");
+                        break;
+
+                }
+
+                SalvaContatos();
+
+                Console.ReadKey();
+            }
+        }
+
+        public void RemoverContato()
+        {
+            Console.Clear();
+
+            int remove;
+            bool opcaoValida3;
+            int removeCorreto;
+
+
+            if (c.Count == 0)
+            {
+                Console.WriteLine("Você não possui contatos.");
+                Console.WriteLine("Clique qualquer tecla para retornar ao menu.");
+                Console.ReadKey();
+            }
+            else
+            {
+                do {
+                    Console.WriteLine("Qual contato deseja remover?");
+                    opcaoValida3 = int.TryParse(Console.ReadLine(), out remove);
+
+                    if (opcaoValida3 == false)
+                    {
+                        Console.WriteLine("Digite apenas o número do contato a ser excluido");
+                    }
+                }while (opcaoValida3 is not true);
+
+                removeCorreto = remove - 1;
+
+                if (removeCorreto < c.Count)
+                {
+                    c.RemoveAt(removeCorreto);
+
+                    Console.WriteLine("Contato removido com sucesso.");
+
+                    Console.WriteLine("Clique qualquer tecla para retornar ao menu.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Esse contato não existe");
+
+                    Console.WriteLine("Clique qualquer tecla para retornar ao menu.");
+                    Console.ReadKey();
+                }
+            }
+
+            SalvaContatos();
+        }
+
+        public void SalvaContatos()
+        {
+           
+            string salvaJson;
+
+            salvaJson = JsonConvert.SerializeObject(c);
+
+            try
+            {
+                System.IO.File.WriteAllText($"{caminho}\\contatos.json", salvaJson);
+
+
+            }catch (Exception ex) 
+            { 
+               Console.WriteLine(ex.Message);
+            }
+
+
+
+        }
+
+        public void CarregaContatos()
+        {
+            bool verificaArquivo;
+
+            verificaArquivo = File.Exists($"{caminho}\\contatos.json");
+
+            if (verificaArquivo)
+            {
+                using (StreamReader sr = new StreamReader($"{caminho}\\contatos.json"))
+                {
+                    var lerJson = sr.ReadToEnd();
+                    var carregaContatos = JsonConvert.DeserializeObject<List<Contatos>>(lerJson);
+
+                    if (carregaContatos == null)
+                    {
+
+                    }
+                    else
+                    {
+                        c = carregaContatos;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há contatos salvos previamente");
+            }
+            
 
         }
     }
